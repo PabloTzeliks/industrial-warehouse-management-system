c935727eb8d06c843c3c6db545fd2058a48ab8a0
PabloTzeliks <arq.pabloo@gmail.com>
2025-10-21 14:12:49 -0300

    feat: adiciona Model de Nota de Entrada

---
+++ src/main/java/pablo/tzeliks/model/NotaEntrada.java
@@ -0,0 +1,10 @@
+package pablo.tzeliks.model;
+
+import java.time.LocalDate;
+
+public class NotaEntrada {
+
+    private int id;
+    private Fornecedor fornecedor;
+    private LocalDate dataEntrada;
+}
